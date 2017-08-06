6c274e89a1e8edd06b7d510af102932d079ff9aa
Elena Dolgova <levinforce@gmail.com>
2017-08-07 01:35:05 +0300

    Add 7 chapter

---
+++ js/6_Chapter/blog.js
@@ -0,0 +1,7 @@
+function init(){
+var planet = document.getElementById("greenplanet");    
+ planet.innerHTML = "Red Alert: hit by phaser fire!"; 
+ planet.setAttribute("class","redtext");
+ }
+
+ window.onload=init;
